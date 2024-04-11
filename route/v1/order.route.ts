5d5e9a4333ebd7e1172c3f84cb847ecaa364ccda
Dwiky Rizky Ananditya <kyzsuki@gnuweeb.org>
2024-04-11 10:12:45 +0700

    refactor(order.route.ts): get order detail

--- route/v1/order.route.ts
+++ route/v1/order.route.ts
@@ -64,4 +64,37 @@ route.post(
   }
 );
 
+route.get(
+  "/get",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    const userId = req.user?.id;
+
+    const db = await conn();
+
+    try {
+      const [orders]: any = await db.execute(
+        "SELECT o.id, o.total_amount, o.address, o.order_date, o.order_status, oi.quantity, oi.price, p.name AS product_name, p.description AS product_description FROM orders o JOIN order_items oi ON o.id = oi.order_id JOIN products p ON oi.product_id = p.id WHERE o.customer_id = ? ORDER BY o.order_date DESC",
+        [userId]
+      );
+
+      const groupedOrders = orders.reduce((acc: any[], order: any) => {
+        const { id, total_amount, address, order_date, ...item } = order;
+        if (!acc[id]) {
+          acc[id] = { id, total_amount, address, order_date, items: [] };
+        }
+        acc[id].items.push(item);
+        return acc;
+      }, {});
+
+      return res.status(200).json(Object.values(groupedOrders));
+    } catch (error) {
+      console.error("Error getting orders:", error);
+      return res.status(500).json({ error: "Internal server error" });
+    } finally {
+      await db.end();
+    }
+  }
+);
+
 export default route;
