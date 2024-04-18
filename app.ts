47a22c683648de0e33b4462dd6023e2b66567bfa
Dwiky Rizky Ananditya <kyzsuki@gnuweeb.org>
2024-04-18 17:07:40 +0700

    refactor(app.ts): add request logging

--- app.ts
+++ app.ts
@@ -3,15 +3,18 @@ import v1Route from "./route/v1";
 import { config } from "dotenv";
 import { passportJwt } from "./lib/passport";
 import cors from "cors";
+import morgan from "morgan";
 
 config();
 
 const port = 3000;
 const app = express();
 
+
 app.use(express.json());
 app.use(cors());
 app.use(passportJwt.initialize());
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
